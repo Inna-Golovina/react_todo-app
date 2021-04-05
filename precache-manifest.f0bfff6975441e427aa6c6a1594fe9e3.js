ba703870e5ef5db9de8ed983a31a61a3a9c19bb5
Inna Golovina <igigig978@gmail.com>
2021-04-05 21:54:46 +0300

    Updates

---
+++ precache-manifest.f0bfff6975441e427aa6c6a1594fe9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b0f4ebe8869567c1d10f2e030dfa8f",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "c0d07b2b30bfaa4f9413",
+    "url": "/react_todo-app/static/css/main.96f8354a.chunk.css"
+  },
+  {
+    "revision": "e56667073a4cb84d11c0",
+    "url": "/react_todo-app/static/js/2.d19f1a2d.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/react_todo-app/static/js/2.d19f1a2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d07b2b30bfaa4f9413",
+    "url": "/react_todo-app/static/js/main.fa37b265.chunk.js"
+  },
+  {
+    "revision": "a7d8665777eba2d482e5",
+    "url": "/react_todo-app/static/js/runtime-main.a08fd161.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9df5a956e84536d6d47a22d845818fea.js"
+  "/react_todo-app/precache-manifest.f0bfff6975441e427aa6c6a1594fe9e3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
